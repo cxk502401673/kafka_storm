52b278bd41b53c2b7275df55125638b154d57aca
chenxiaokai <502401673@qq.com>
2018-03-28 18:17:35 +0800

    添加注释

--- src/main/java/com/cxk/producter/MyProducer.java
+++ src/main/java/com/cxk/producter/MyProducer.java
@@ -18,6 +18,13 @@ public class MyProducer {
     public Properties createProperties() {
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");
+//        当ack=0的时候，表示producer发送出去message，只要对应的kafka broker topic partition leader接收到的这条message，
+//        producer就返回成功，不管partition leader 是否真的成功把message真正存到kafka。
+//        当ack=1的时候，表示producer发送出去message，同步的把message存到对应topic的partition的leader上，
+//        然后producer就返回成功，partition leader异步的把message同步到其他partition replica上。
+//        当ack=all或-1，表示producer发送出去message，同步的把message存到对应topic的partition的leader和对应的replica上之后，
+//        才返回成功。但是如果某个kafka controller 切换的时候，会导致partition leader的切换（
+//        老的 kafka controller上面的partition leader会选举到其他的kafka broker上）,但是这样就会导致丢数据。
         properties.put("acks", "all");
         properties.put("retries", 0); // 消息发送请求失败重试次数
         properties.put("batch.size", 2000);
@@ -26,8 +33,11 @@ public class MyProducer {
         // 如果发送到不同分区，并且不想采用默认的Utils.abs(key.hashCode) % numPartitions分区方式，则需要自己自定义分区逻辑
         // properties.put("partitioner.class", "com.cxk.producter.MyPartitioner");
         properties.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
+        //指定序列化方式
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+
         return properties;
     }
 
